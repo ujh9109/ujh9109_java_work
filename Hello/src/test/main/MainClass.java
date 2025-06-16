788f5a52af76a0e461567591c532fee5102a8df3
yujaehwang <ujh9109@gmail.com>
2025-06-16 17:06:08 +0900

    메인 클래스 05 추가

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,40 @@
+package test.main;
+
+// MainClass 라는 이름의 클래스 만들기 
+public class MainClass {
+	
+	// main 이라는 이름의 메소드를 클래스 안에 만들기 
+	public static void main(String[] args) {
+		// main() 메소드 안에 있는 코드를 실행하려면 ▶ 버튼을 누르면 된다
+		System.out.println("Hello, World!");
+		System.out.println("김구라");
+		
+		// let num=10;
+		int num=10;
+		num=20;
+		num=30;
+		// type 이 맞지 않으면 안들어간다. 
+		//num=10.1;
+		//num=true;
+		//num="abcd";
+		double num2=10.1;
+		num2=20.1;
+		
+		boolean isRun=true;
+		isRun=false;
+		
+		String myName="김구라";
+		myName="해골";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
---
+++ step01_DataType/src/test/main/MainClass05.java
@@ -0,0 +1,41 @@
+package test.main;
+
+/*
+ * [자바 참조 데이터 타입]
+ * 
+ * string type
+ * 
+ * 문자열을 다룰떄 사용하는 데이터 타입이다.
+ * 
+ */
+
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		//"김구라" 라는 string type 객체를 heap 영역에 만들고 그 참조값을 변수에 담기
+		String name = "김구라";
+		//name 안에 있는 참조값을 tmp 변수에 복사해 주기
+		String tmp = name;
+		//"원숭이"라는 string type 객체를 heap 영역에 만들고 그 참조값을 name 변수에 덮어쓰기
+		name = "원숭이";
+		//name 변수를  비우기 / null은 참조 데이터 type이 담길 수 있는 빈 공간을 의미한다.
+		name = null;
+		
+		
+		//변수 안에 참조값이 들어가는게 아니고, 실제 값이 들어가는 기본 데이터 타입.
+		int num = 10;
+		double num2 = 10.1;
+		boolean isRun = true;
+		
+		//변수 안에 참조값이 들어가는 참조 데이터 타입인 string
+		String yourName = "해골";
+		
+		yourName.
+		
+		num.
+		
+		
+		}
+
+	
+}
