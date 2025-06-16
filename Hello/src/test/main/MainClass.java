6dd164ec30c47881b127fbf316e1dc800c091327
yujaehwang <ujh9109@gmail.com>
2025-06-16 16:11:42 +0900

    hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,89 @@
+package test.main;
+
+//MainClass 라는 이름의 클래스 만들기
+public class MainClass {
+	
+	//main이라는 이름의 메소드를 클래스 안에 만들기
+	public static void main(String[] args) {
+		//main() 메소드 안에 있는 코드를 실행하려면 run 버튼 ▶을 누르면 된다
+			System.out.println("Hello, World!");
+			System.out.println("유재황");
+			
+			//let num = 10 <- 자바스크립트
+			int num = 10;
+			num = 20;
+			num = 30;
+			
+			double num2 = 10.1;
+			num2 = 20.1;
+			
+			boolean isRun = true;
+			isRun = false;
+			
+			String myName = "유재황";
+			myName = "yujaehwang";
+		
+			int number;
+			number = 10;
+			int age = 25;
+			
+			final int MAX_VALUE = 100;
+			
+			
+			}
+			
+		}
+
+/*
+
+public class MyClass {
+	public static final double PI = 3.14159;}
+
+public class Example {
+	public static final int MAX_SCORE = 100;
+	
+	public static void main(string[] args) {
+		int score = 90;
+		System.out.println("score: " + score);
+		System.out.println("Max Score: " + MAX_SCORE);
+		
+		int a = 10;
+		int b = 3;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		int a = 5;
+		int b = 10;
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		
+		bollean a = true;
+		bollean b = false;
+		System.out.println(a&&b);
+		System.out.println(a||b);
+		System.out.println(!a);
+		
+		int a = 10;
+		a += 5;
+		a -= 3;
+		a *= 2;
+		a /= 4;
+		a %= 5;
+		
+		int a = 10;
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println)(--a);
+		
+		
+	}
+}
+*/
